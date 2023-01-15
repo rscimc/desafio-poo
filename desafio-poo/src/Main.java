3902e85ac30e9005904ab7b32d2c785ff1885600
Ricardo <rscimc@gmail.com>
2023-01-15 20:52:21 -0300

    Created classes - Mentoria and Cursos.

---
+++ desafio-poo/src/Main.java
@@ -0,0 +1,14 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Curso curso1 = new Curso();
+		Mentoria mentoria1 = new Mentoria();
+		
+		System.out.println(curso1);
+		System.out.println(mentoria1);
+	}
+
+}
